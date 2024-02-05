5bd38c14672f763e151abfa46a903b72bc09ad9b
Murloc6 <881739+Murloc6@users.noreply.github.com>
2024-02-05 16:02:48 +0100

    fix: Remove prefix from route definition since it is define in the blueprint

--- tdd_api_plugin_aas/__init__.py
+++ tdd_api_plugin_aas/__init__.py
@@ -12,6 +12,7 @@ from tdd.utils import (
 )
 from tdd.errors import WrongMimeType
 
+
 from tdd_api_plugin_aas.aas import (
     get_aas_description,
     put_aas_json_in_sparql,
@@ -21,14 +22,15 @@ from tdd_api_plugin_aas.aas import (
 
 blueprint = Blueprint('tdd_api_plugin_aas', __name__, url_prefix="/aas")
 
-@blueprint.route("/aas/<id>", methods=["DELETE"])
+
+@blueprint.route("/<id>", methods=["DELETE"])
 def delete_route_aas(id):
     response = delete_id(id)
     if response.status_code in [200, 204]:
         update_collection_etag()
     return response
 
-@blueprint.route("/aas/<id>", methods=["GET"])
+@blueprint.route("/<id>", methods=["GET"])
 def describe_aas(id):
     mime_type_negociated = negociate_mime_type(
         request, default_mimetype="application/aas+json"
@@ -38,7 +40,7 @@ def describe_aas(id):
         description = json.dumps(description)
     return Response(description, content_type=mime_type_negociated)
 
-@blueprint.route("/aas/<id>", methods=["PUT"])
+@blueprint.route("/<id>", methods=["PUT"])
 def create_aas(id):
     mimetype = request.content_type
     check_schema = get_check_schema_from_url_params(request)
@@ -57,7 +59,7 @@ def create_aas(id):
     update_collection_etag()
     return Response(status=201 if not updated else 204, headers={"Location": uri})
 
-@blueprint.route("/aas", methods=["POST"])
+@blueprint.route("/", methods=["POST"])
 def create_anonymous_aas():
     mimetype = request.content_type
     check_schema = get_check_schema_from_url_params(request)
